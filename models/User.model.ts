212de450e1d605aac5490ef12a38d7704653c025
EricKogut <40190283+EricKogut@users.noreply.github.com>
2021-08-25 15:00:44 -0400

    Create User.model.ts

---
+++ models/User.model.ts
@@ -0,0 +1,30 @@
+import { Schema, Model, model } from "mongoose";
+
+interface UserDocument {
+  username: string;
+  first_name: string;
+  last_name: string;
+  password: string;
+  salt: string;
+  email: string;
+  bio: string;
+  picture: string;
+}
+
+const UserSchema = new Schema(
+  {
+    username: { type: Schema.Types.String, required: true, unique: true },
+    first_name: { type: Schema.Types.String, required: true, unique: true },
+    last_name: { type: Schema.Types.String, required: true, unique: true },
+    password: { type: Schema.Types.String, required: true },
+    salt: { type: Schema.Types.String, required: true },
+    email: { type: Schema.Types.String, required: true, unique: true },
+    bio: Schema.Types.String,
+    picture: Schema.Types.String,
+  },
+  { timestamps: true }
+);
+
+const User: Model<UserDocument> = model<UserDocument>("user", UserSchema);
+
+export { UserDocument, User };
