befcc2602cc00f54c7cd693fbb3a6857b3ec23da
EricKogut <eric.jkogut@gmail.com>
2021-09-15 21:22:31 -0400

    Update server.ts
    - Adding cors handling

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
-
 // Importing deps
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 import { Router, Request, Response, NextFunction } from "express";
 const bodyParser = require("body-parser");
+var cors = require("cors");
 
 // Declaring mongo database
 import { connectDB } from "./db";
@@ -37,6 +37,22 @@ const server = app.listen(port, () =>
 // Creating DB Connection
 connectDB();
 
+// Cors (found the nice explanatino on "https://stackoverflow.com/questions/56798251/node-js-with-angular-cors-error-when-i-send-request")
+app.use(
+  cors({
+    origin: true, // "true" will copy the domain of the request back
+    // to the reply. If you need more control than this
+    // use a function.
+
+    credentials: true, // This MUST be "true" if your endpoint is
+    // authenticated via either a session cookie
+    // or Authorization header. Otherwise the
+    // browser will block the response.
+
+    methods: "POST,GET,PUT,OPTIONS,DELETE", // Make sure you're not blocking
+    // pre-flight OPTIONS requests
+  })
+);
 // Default display
 app.get("/", (req: Request, res: Response) =>
   res.sendFile(__dirname + "/index.html")
